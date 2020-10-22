a709f44be6c9554206bd10322edfc74f99af5261
jdrichardsmern <jd.richards@codeimmersives.com>
2020-10-22 10:56:20 -0400

    added functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
@@ -31,6 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/Words/wordRoutes.js
+++ routes/Words/wordRoutes.js
@@ -46,4 +46,68 @@ router.get('/add-word', (req, res) => {
   return res.render('main/add-word');
 });
 
+router.get('/single-word/:wordId', (req, res) => {
+  Word.findById(req.params.wordId)
+    .then((dbWord) => {
+      if (dbWord) {
+        return res.render('main/single-word', { foundWord: dbWord });
+      } else {
+        return res.status(400).send('No Word Found');
+      }
+    })
+    .catch((err) => {
+      return res
+        .status(500)
+        .json({ confirmation: 'fail', message: 'Server Error', err });
+    });
+});
+
+router.get('/update/:wordId', (req, res) => {
+  Word.findById(req.params.wordId)
+    .then((dbWord) => {
+      if (!dbWord) {
+        return res.status(400).send('No Word Found');
+      }
+      return res.status(200).render('main/update-word', { foundWord: dbWord });
+    })
+    .catch((err) => {
+      return res.status(500).json({ message: 'Server Error' });
+    });
+});
+
+router.put('/update/:wordId', (req, res) => {
+  Word.findById(req.params.wordId)
+    .then((foundWord) => {
+      if (!foundWord) {
+        return res.status(400).send('No Word Found');
+      }
+      if (!req.body.meaning) {
+        return res.status(400).send('All Inputs Must Be Filled');
+      }
+
+      foundWord.meaning = req.body.meaning;
+
+      foundWord.save().then(() => {
+        return res.redirect(`/words/single-word/${req.params.wordId}`);
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({ message: 'Server Error' });
+    });
+});
+
+router.delete('/delete/:wordId', (req, res) => {
+  Word.findByIdAndDelete(req.params.wordId)
+    .then((foundWord) => {
+      if (!foundWord) {
+        return res.status(400).send('Word Not Found');
+      }
+
+      return res.status(200).redirect('/words/get-words');
+    })
+    .catch((err) => {
+      return res.status(200).json({ message: 'Server Error' });
+    });
+});
+
 module.exports = router;
